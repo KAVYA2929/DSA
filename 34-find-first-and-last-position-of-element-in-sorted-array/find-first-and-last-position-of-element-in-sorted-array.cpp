be05f851b894fd15e09b57bcf8a7b906616e8dc5
KAVYA2929 <144532671+KAVYA2929@users.noreply.github.com>
2025-12-16 16:25:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (51.45%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,17 +1,47 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-         int first = -1, last = -1;
-
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] == target) {
-            if (first == -1) {
-                first = i;   // first occurrence
-            }
-            last = i;        // last occurrence
-        }
-    }
-    return {first, last};
+int lowerbound(vector<int> &arr,int n , int y){
+	int low = 0;
+	int high = n -1;
+	int ans = n;
+	while(low<=high){
+		int mid = (high + low) / 2;
+		if(arr[mid] >= y){
+			ans = mid;
+			high = mid - 1;
+			
+		}
+		else{
+			low = mid + 1;
+		}
+		
+	}
+	return ans;
 }
+int upperbound(vector<int> &arr,int n , int k){
+	int low = 0;
+	int high = n -1;
+	int ans = n;
+	while(low<=high){
+		int mid = (high + low) / 2;
+		if(arr[mid] > k){
+			ans = mid;
+			high = mid - 1;
+			
+		}
+		else{
+			low = mid + 1;
+		}
+		
+
+	}
+	return ans;
+}
+    vector<int> searchRange(vector<int>& nums, int target) {
+       int n = nums.size();
+	int lb = lowerbound(nums ,n,target );
+	if(lb == n || nums[lb] != target) return {-1,-1};
+	return{lb , upperbound(nums,n,target)-1};
+    }
     
 };
