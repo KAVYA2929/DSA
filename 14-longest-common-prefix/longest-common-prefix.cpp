65b06922bedaead3b2d4fe39c5ef9c9898447b17
kavya agrawal <144532671+KAVYA2929@users.noreply.github.com>
2026-06-24 05:19:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (40.42%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+        for(int i = 0;i<strs[0].length();i++){
+            char ch = strs[0][i];
+            for(int j = 1;j<strs.size();j++){
+                if(strs[j][i] != ch || i >strs[j].length()){
+                    return ans;
+                }
+               
+            }
+             ans = ans + ch;
+        }
+        return ans;
+        
+    }
+};
