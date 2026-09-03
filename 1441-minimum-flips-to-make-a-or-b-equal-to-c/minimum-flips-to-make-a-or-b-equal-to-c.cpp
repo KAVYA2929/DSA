c7cf2b4040ad90c40b22756783795cc0680140f5
kavya agrawal <144532671+KAVYA2929@users.noreply.github.com>
2026-09-03 11:59:38 +0530

    Time: 2 ms (54.09%) | Memory: 7.7 MB (68.78%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int flip = 0;
-        while(a != 0 || b != 0 || c != 0){
-            if((c & 1) == 1){
-                if((a & 1) == 0 && (b & 1) == 0){
-                    flip++;
-                }
-            }else{
-                if(a & 1 == 1){
-                    flip++;
-                }
-                if(b & 1 == 1){
-                    flip++;
-                }
-            }
-            a>>=1;
-            b>>=1;
-            c>>=1;
-        }
-        return flip;
+        int result = ((a |b)^c);
+        int result1 = (a&b);
+        int result2 = (result & result1);
+        return(__builtin_popcount(result) + __builtin_popcount(result2));
         
     }
 };
